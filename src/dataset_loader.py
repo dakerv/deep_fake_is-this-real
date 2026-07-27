aef842c958439785c75353cd2be27149e70a5273
dakerv <vedaker@st.ug.edu.gh>
2026-07-27 00:04:42 +0000

    Function for transformations is ready, plus updated docstring

--- src/dataset_loader.py
+++ src/dataset_loader.py
@@ -3,6 +3,34 @@ from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 
+"""
+Current folder structure
+------------------------
+
+dataset/
+
+├── train/
+│   ├── real/
+│   ├── synthetic/
+│   └── swapped/
+│
+├── val/
+│   ├── real/
+│   ├── synthetic/
+│   └── swapped/
+│
+└── test/
+    ├── real/
+    ├── synthetic/
+    └── swapped/
+
+Pytorch needs code that answers three questions:
+1. How many images exist?
+2. Give an image number - returns (image, label)
+3. Convert image to format a CNN expects - channels x height x width
+
+"""
+
 class DeepfakeDataset(Dataset):
     def __init__(self, root_dir, transform=None):
         self.root_dir = Path(root_dir)
@@ -47,3 +75,46 @@ class DeepfakeDataset(Dataset):
             image = self.transform(image)
 
         return image, label
+
+    def get_transforms():
+        train_transform = transforms.Compose(
+            [
+               transforms.RandomHorizontalFlip(),
+               transforms.RandomRotation(10),
+
+               transforms.ToTensor(),
+
+               transforms.Normalize(
+                   mean= [
+                       0.485,
+                       0.456,
+                       0.406
+                   ],
+                   std=[
+                       0.229,
+                       0.224,
+                       0.225
+                   ]
+               )
+            ]
+        )
+
+        val_transform = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    mean=[
+                        0.485,
+                        0.456,
+                        0.406
+                    ],
+                    std=[
+                        0.229,
+                        0.224,
+                        0.225
+                    ]
+                )
+            ]
+        )
+
+        return train_transform, val_transform
