fff33f59637620aaa5db2ebb4485cab5e3319bc5
dakerv <vedaker@st.ug.edu.gh>
2026-08-27 01:39:25 +0000

    Requests

--- test_request.py
+++ test_request.py
@@ -1,7 +1,13 @@
 import requests
 
-response = requests.post(
-    "http://127.0.0.1:5000/predict"
+IMAGE_PATH = "/deep_fake_is-this-real/dataset/test/real/02262.jpg"
+
+with open(IMAGE_PATH, "rb") as image_file:
+    response = requests.post(
+    "http://127.0.0.1:5000/predict",
+    files={
+        "image": image_file
+    }
 )
 
 print("Status code:", response.status_code)
